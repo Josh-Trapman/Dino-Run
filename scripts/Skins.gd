c343245dde825bdf7c202c83a609efd6350fec3f
Josh Trapman <joshuatrapman1@gmail.com>
2024-10-05 22:42:52 +1000

    Implemented all background skin work

--- scripts/Skins.gd
+++ scripts/Skins.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var buttons = get_parent().find_child("Buttons")
 
+# Equips Blue skin
 func _on_blue_equip_pressed():
 	Global.player_data["UserData"]["EquippedSkin"] = "Bluezoid"
 	Global.game_instance.save_data()
@@ -9,6 +10,8 @@ func _on_blue_equip_pressed():
 	$SkinBlue/SkinEquipped.visible = true
 	$SkinBlue/EquipSkin.visible = false
 
+
+# Buys Red skin
 func _on_red_buy_pressed():
 	var current_coins = Global.player_data["UserData"]["Coins"]
 	# Reduces the players coin balance and unlocks the skin
@@ -22,6 +25,8 @@ func _on_red_buy_pressed():
 		$SkinRed/Buy.visible = false
 		$SkinRed/EquipSkin.visible = true
 
+
+# Equips Red skin
 func _on_red_equip_pressed():
 	Global.player_data["UserData"]["EquippedSkin"] = "BlazeRex"
 	Global.game_instance.save_data()
@@ -30,20 +35,38 @@ func _on_red_equip_pressed():
 	$SkinRed/EquipSkin.visible = false
 
 
+# Buys Green skin
 func _on_green_buy_pressed():
-	pass # Replace with function body.
+	var current_coins = Global.player_data["UserData"]["Coins"]
+	# Reduces the players coin balance and unlocks the skin
+	if Global.player_data["UserData"]["Coins"] >= 500 and Global.player_data["UserData"]["Skins"]["LeafRex"] == false:
+		Global.player_data["UserData"]["Coins"] = current_coins - 500
+		Global.player_data["UserData"]["Skins"]["LeafRex"] = true
+		Global.game_instance.save_data()
+		
+		# Sets the skin to an equippable state
+		$SkinGreen/Price.visible = false
+		$SkinGreen/Buy.visible = false
+		$SkinGreen/EquipSkin.visible = true
 
 
+# Equips Green skin
 func _on_green_equip_pressed():
-	pass # Replace with function body.
+	Global.player_data["UserData"]["EquippedSkin"] = "LeafRex"
+	Global.game_instance.save_data()
+	
+	$SkinGreen/SkinEquipped.visible = true
+	$SkinGreen/EquipSkin.visible = false
 
 
+# Closes page
 func _on_back_pressed():
 	visible = false
 	Global.can_click = true
 	buttons.visible = true
 
 
+# Updates the page according to player data on log in
 func load_in():
 	# Set Blue skin to equipped state
 	if Global.player_data["UserData"]["EquippedSkin"] == "Bluezoid":
