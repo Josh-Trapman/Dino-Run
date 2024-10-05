c2a5b9755c9c84437534df8685dd432666ff2de2
Josh Trapman <joshuatrapman1@gmail.com>
2024-10-05 17:36:20 +1000

    Initial-Commit

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,13 +2,10 @@ extends Node
 
 @onready var game = preload("res://scenes/game.tscn")
 
-var player_data : Dictionary
-var player_key
 var save_path = "user://save.dat"
-var game_instance
 
-func logged_in(account : Dictionary, key):
-	player_data = account
-	player_key = key
-	game_instance = game.instantiate()
-	add_child(game_instance)
+
+func logged_in():
+	Global.game_instance = game.instantiate()
+	print(Global.game_instance)
+	add_child(Global.game_instance)
---
+++ scripts/Skins.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var buttons = get_parent().find_child("Buttons")
+
+func _on_blue_equip_pressed():
+	$SkinBlue/SkinEquipped.visible = true
+	$SkinBlue/EquipSkin.visible = false
+
+
+func _on_red_buy_pressed():
+	pass # Replace with function body.
+
+
+func _on_red_equip_pressed():
+	pass # Replace with function body.
+
+
+func _on_green_buy_pressed():
+	pass # Replace with function body.
+
+
+func _on_green_equip_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	visible = false
+	Global.can_click = true
+	buttons.visible = true
